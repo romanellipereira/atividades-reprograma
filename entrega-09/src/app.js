f034d15a91f5bb5a02322039edbbb8cdd824afc7
romanellipereira <romanelli.pereira@gmail.com>
2020-10-05 20:01:06 -0300

    initial commit

---
+++ entrega-09/src/app.js
@@ -0,0 +1,14 @@
+// instâncias
+const express = require("express");
+const app = express();
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    )
+    next()
+  });
+
+module.exports = app;
